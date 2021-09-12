0d22eabd22aea44468a4358e59a3cc53035911a3
techhelper2 <84462159+techhelper2@users.noreply.github.com>
2021-09-12 10:48:10 +0530

    Update start.py

--- assistant/start.py
+++ assistant/start.py
@@ -23,7 +23,7 @@ Owner_info_msg = f"""
 
 <strong>Message Forwards</strong> - {udB.get("PMBOT")}
 
-<stong>Ultroid <a href=https://github.com/TeamUltroid/Ultroid>[v{ultroid_version}]</a>, powered by @TeamUltroid</strong>
+<stong>Made by <a href=https://t.me/geronimo1234>[UncleG]</a></strong>
 """
 
 _settings = [
